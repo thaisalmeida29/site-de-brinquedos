bc8336114590dffa16e14b06681fffbe4a69f587
thaisalmeida29 <thaisalmeida.galvao@gmail.com>
2022-08-23 19:25:22 -0300

    primeiro commit

---
+++ serginho-diversoes/Models/Brinquedo.cs
@@ -0,0 +1,18 @@
+﻿namespace serginho_diversoes.Models
+{
+    public class Brinquedo
+    {
+        public int BrinquedoId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool BrinquedoPreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }    
+    }
+}
---
+++ serginho-diversoes/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿namespace serginho_diversoes.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }   
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        public List<Brinquedo> Brinquedos { get; set; }
+    }
+}
